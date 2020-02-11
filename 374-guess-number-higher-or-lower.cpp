f2c3bbdc14fc25f0d2539c3bb14dd423c0ad8f0b
Nothing <wwwaap@gmail.com>
2020-02-11 09:12:06 +0800

    Create 374-guess-number-higher-or-lower.cpp

---
+++ 374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,22 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+  public:
+    int guessNumber(int n) {
+      int maxNumber = n, minNumber = 1;
+      while (true) {
+        int meanNumber = (maxNumber - minNumber) / 2 + minNumber;
+        int res = guess(meanNumber);
+        if (res == 0) {
+          return meanNumber;
+        } else if (res == 1) {
+          minNumber = meanNumber + 1;
+        } else {
+          maxNumber = meanNumber - 1;
+        }
+      }
+    }
+};
